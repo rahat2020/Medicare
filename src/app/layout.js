9f9c7ad3e89cf0b3e79baadc51338fc4dfb6806d
rahat2020 <kazimdboktiar2020@gmail.com>
2024-09-14 00:33:17 +0600

    tailwind config setup completed and edite root layout and create public folder for assets

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,10 @@
-import localFont from "next/font/local";
 import "./globals.css";
+import { Montserrat } from 'next/font/google';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const montserrat = Montserrat({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+  display: 'swap'
 });
 
 export const metadata = {
@@ -21,7 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={montserrat.className}
       >
         {children}
       </body>
