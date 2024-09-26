42a53791198add3664f0dfb86c59c2af2d459d5c
Jisan Molla <jisanahamed09205@gmail.com>
2024-09-26 12:39:09 +0600

    2nd push test

--- src/components/Home/ServiceCategory.jsx
+++ src/components/Home/ServiceCategory.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ServiceCategory = () => {
   return (
-    <div>ServiceCategory Specialities</div>
+    <div>ServiceCategory Specialities now this test is 2nd time pushing test it is work or not</div>
   )
 }
 
