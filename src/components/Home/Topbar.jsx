c8547ad49e43e796adb49e3a4bab8985f30613d7
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-27 22:53:35 +0600

    Navbar navigation link's show dropdown menu on navbar links hover styles added

--- src/components/Home/Topbar.jsx
+++ src/components/Home/Topbar.jsx
@@ -8,27 +8,106 @@ const Topbar = () => {
   const links = (
     <>
       <Link href="/" className="hover:text-blue-600 flex gap-1 items-center">
-        Home <IoIosArrowDown/>
+        Home
       </Link>
-      <Link href="/pages" className="hover:text-blue-600 flex gap-1 items-center">
 
-        Pages <IoIosArrowDown/>
+      <div className="relative group">
+        <Link
+          href="/pages"
+          className="hover:text-blue-600 flex gap-1 items-center"
+        >
+          Pages <IoIosArrowDown />
+        </Link>
+
+        {/* Dropdown */}
+        <div className="w-48 absolute top-6 border rounded-sm opacity-0 group-hover:opacity-100 group-hover:translate-y-0 translate-y-2 transition-all duration-700 ease-in-out hidden group-hover:block bg-white shadow-lg">
+          <Link
+            href="/about"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 hover:px-6 duration-300"
+          >
+            About Us
+          </Link>
+          <hr />
+          <Link
+            href="/services"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 hover:px-6 duration-300"
+          >
+            Services
+          </Link>
+          <hr />
+          <Link
+            href="/serviceDetails"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 hover:px-6 duration-300"
+          >
+            Service Details
+          </Link>
+          <hr />
+          <Link
+            href="/faq"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 hover:px-6 duration-300"
+          >
+            FAQ
+          </Link>
+        </div>
+      </div>
+
+      <div className="relative group">
+        <Link
+          href="/specialties"
+          className="hover:text-blue-600 flex gap-1 items-center"
+        >
+          Specialties <IoIosArrowDown />
+        </Link>
+
+        {/* Dropdown */}
+        <div className="w-48 absolute top-6 border rounded-sm opacity-0 group-hover:opacity-100 group-hover:translate-y-0 translate-y-2 transition-all duration-700 ease-in-out hidden group-hover:block bg-white shadow-lg">
+          <Link
+            href="/cardiologist"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 hover:px-6 duration-300"
+          >
+            Cardiologist
+          </Link>
+          <hr />
+          <Link
+            href="/neurology"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 hover:px-6 duration-300"
+          >
+            Neurology
+          </Link>
+          <hr />
+          <Link
+            href="/ophthalmology"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 hover:px-6 duration-300"
+          >
+            Ophthalmology
+          </Link>
+          <hr />
+          <Link
+            href="/urology"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 hover:px-6 duration-300"
+          >
+            Urology
+          </Link>
+        </div>
+      </div>
+
+      <Link
+        href="/doctors"
+        className="hover:text-blue-600 flex gap-1 items-center"
+      >
+        Doctors
       </Link>
-      <Link href="/specialties" className="hover:text-blue-600 flex gap-1 items-center">
-
-        Specialties <IoIosArrowDown/>
+      <Link
+        href="/blog"
+        className="hover:text-blue-600 flex gap-1 items-center"
+      >
+        Blog
       </Link>
-      <Link href="/doctors" className="hover:text-blue-600 flex gap-1 items-center">
-
-        Doctors <IoIosArrowDown/>
-      </Link>
-      <Link href="/blog" className="hover:text-blue-600 flex gap-1 items-center">
-
-        Blog <IoIosArrowDown/>
-      </Link>
-      <Link href="/contact" className="hover:text-blue-600 flex gap-1 items-center">
-
-        Cont act Us <IoIosArrowDown/>
+      <Link
+        href="/contact"
+        className="hover:text-blue-600 flex gap-1 items-center"
+      >
+        Contact Us
       </Link>
     </>
   );
@@ -42,7 +121,7 @@ const Topbar = () => {
         </div>
 
         {/* Navigation Links */}
-        <div className="hidden md:flex space-x-6 font-semibold duration-300">
+        <div className="hidden md:flex space-x-6 font-[500] duration-300">
           {links}
         </div>
 
