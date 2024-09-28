b3160ea5ff695d450abda691107fb8ffeae47780
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-09-28 15:49:27 +0600

    Medicare navbar completed

--- src/components/Home/Topbar.jsx
+++ src/components/Home/Topbar.jsx
@@ -1,13 +1,12 @@
-'use client'
+"use client";
 import Link from "next/link";
 import React, { useState } from "react";
-import { FaRegUser } from "react-icons/fa"; // Updated import
-import { IoLockClosedOutline } from "react-icons/io5";
-import { IoIosArrowDown } from "react-icons/io";
-import { IoMenu, IoClose } from "react-icons/io5";
+import { Menu, X, User, Lock, ChevronDown } from "react-feather";
 
 const Topbar = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [pagesDropdownOpen, setPagesDropdownOpen] = useState(false);
+  const [speDropdownOpen, setSpeDropdownOpen] = useState(false);
 
   const links = (
     <>
@@ -16,14 +15,16 @@ const Topbar = () => {
       </Link>
 
       <div className="relative group">
-        <Link href={"#"}
+        <Link
+          href={"#"}
           className="hover:text-blue-600 flex gap-1 items-center"
         >
-          Pages <IoIosArrowDown />
+          Pages <ChevronDown />
         </Link>
 
         {/* Dropdown */}
-        <div className="w-48 absolute left-0 top-full mt-1 border rounded-sm opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out hidden group-hover:block bg-white shadow-lg z-10">
+        <div className="w-48 absolute left-0 top-6 mt-1 border rounded-sm opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out hidden group-hover:block bg-white shadow-lg z-10">
+          <div className="bg-white w-48 py-2 opacity-0 -mt-4"></div>
           <Link
             href="/about"
             className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
@@ -55,15 +56,13 @@ const Topbar = () => {
       </div>
 
       <div className="relative group">
-        <Link
-          href="#"
-          className="hover:text-blue-600 flex gap-1 items-center"
-        >
-          Specialties <IoIosArrowDown />
+        <Link href="#" className="hover:text-blue-600 flex gap-1 items-center">
+          Specialties <ChevronDown />
         </Link>
 
         {/* Dropdown */}
-        <div className="w-48 absolute left-0 top-full mt-1 border rounded-sm opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out hidden group-hover:block bg-white shadow-lg z-10">
+        <div className="w-48 absolute left-0 top-6 mt-1 border rounded-sm opacity-0 group-hover:opacity-100 transition-all duration-300 ease-in-out hidden group-hover:block bg-white shadow-lg z-10">
+          <div className="bg-white w-48 py-2 opacity-0 -mt-4"></div>
           <Link
             href="/cardiologist"
             className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
@@ -114,52 +113,158 @@ const Topbar = () => {
       </Link>
     </>
   );
+  const smallDeviceLinks = (
+    <>
+      <Link href="/" className="hover:text-blue-600 flex gap-1 items-center">
+        Home
+      </Link>
+
+      <div className="relative">
+        <Link
+          href={"#"}
+          onClick={() => setPagesDropdownOpen(!pagesDropdownOpen)}
+          className="hover:text-blue-600 flex gap-1 items-center"
+        >
+          Pages <ChevronDown />
+        </Link>
+
+        {/* Dropdown */}
+        {
+        pagesDropdownOpen && <div className="w-48 left-0 top-6 mt-1 border rounded-sm transition-all duration-300 bg-white shadow-lg z-10">
+          <Link
+            href="/about"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
+          >
+            About Us
+          </Link>
+          <hr />
+          <Link
+            href="/services"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
+          >
+            Services
+          </Link>
+          <hr />
+          <Link
+            href="/serviceDetails"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
+          >
+            Service Details
+          </Link>
+          <hr />
+          <Link
+            href="/faq"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
+          >
+            FAQ
+          </Link>
+        </div>
+        }
+      </div>
+
+      <div className="relative">
+        <Link onClick={()=> setSpeDropdownOpen(!speDropdownOpen)} href="#" className="hover:text-blue-600 flex gap-1 items-center">
+          Specialties <ChevronDown />
+        </Link>
+
+        {/* Dropdown */}
+        {speDropdownOpen && <div className="w-48 left-0 top-6 mt-1 border rounded-sm transition-all duration-300 bg-white shadow-lg z-10">
+          <div className="bg-white w-48 py-2 opacity-0 -mt-4"></div>
+          <Link
+            href="/cardiologist"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
+          >
+            Cardiologist
+          </Link>
+          <hr />
+          <Link
+            href="/neurology"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
+          >
+            Neurology
+          </Link>
+          <hr />
+          <Link
+            href="/ophthalmology"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
+          >
+            Ophthalmology
+          </Link>
+          <hr />
+          <Link
+            href="/urology"
+            className="hover:text-blue-600 flex gap-1 items-center px-4 py-3 duration-300"
+          >
+            Urology
+          </Link>
+        </div>}
+      </div>
+
+      <Link
+        href="/doctors"
+        className="hover:text-blue-600 flex gap-1 items-center"
+      >
+        Doctors
+      </Link>
+      <Link
+        href="/blog"
+        className="hover:text-blue-600 flex gap-1 items-center"
+      >
+        Blog
+      </Link>
+      <Link
+        href="/contact"
+        className="hover:text-blue-600 flex gap-1 items-center"
+      >
+        Contact Us
+      </Link>
+    </>
+  );
 
   return (
     <nav className="bg-white">
-      <div className="md:container mx-auto flex items-center justify-between p-4 text-[15px]">
-        <div className="flex gap-4">
-        {/* Mobile Menu Button */}
-        <button
-          className="md:hidden text-3xl text-blue-600"
-          onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+      <div className="px-4 md:px-6 lg:px-0 flex items-center justify-between text-sm md:text-[15px] py-4">
+        <div className="flex gap-4 items-center">
+          {/* Mobile Menu Button */}
+          <button
+            className="md:hidden text-3xl text-blue-600"
+            onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
           >
-          {isMobileMenuOpen ? <IoClose /> : <IoMenu />}
-        </button>
-        
+            {isMobileMenuOpen ? <X /> : <Menu />}
+          </button>
+
           {/* Logo */}
-        <div className="text-3xl font-bold text-blue-600">
-          <Link href="/">Medicare</Link>
-        </div>
+          <div className="text-3xl font-bold text-blue-600">
+            <Link href="/">Medicare</Link>
+          </div>
         </div>
 
         {/* Desktop Navigation Links */}
-        <div className="hidden md:flex space-x-6 font-[500] duration-300">
+        <div className="hidden md:flex space-x-3 lg:space-x-6 font-[500] flex-grow justify-center">
           {links}
         </div>
 
-        {/* Contact Info and Buttons */}
-        <div className="hidden md:flex items-center space-x-4">
+        {/* Buttons */}
+        <div className="hidden md:flex items-center space-x-2 md:space-x-4">
           <Link
             href="/signUp"
-            className="hover:bg-blue-600 hover:text-white px-4 py-2 rounded-md border font-semibold flex items-center gap-2 duration-300"
+            className="hover:bg-blue-600 hover:text-white px-2 lg:px-4 py-1 lg:py-2 rounded-md border font-semibold flex items-center gap-2 duration-300"
           >
-            <FaRegUser /> Register
+            <User /> Register
           </Link>
           <Link
             href="/login"
-            className="bg-blue-600 text-white hover:bg-white hover:text-blue-600 px-4 py-2 rounded-md border font-semibold flex items-center gap-2 duration-300"
+            className="bg-blue-600 text-white hover:bg-white hover:text-blue-600 px-2 lg:px-4 py-1 lg:py-2 rounded-md border font-semibold flex items-center gap-2 duration-300"
           >
-            <IoLockClosedOutline className="font-extra-bold text-16" /> Login
+            <Lock className="font-extra-bold text-sm md:text-16" /> Login
           </Link>
         </div>
-
       </div>
 
       {/* Mobile Menu */}
       {isMobileMenuOpen && (
         <div className="md:hidden bg-white shadow-lg p-4 space-y-4">
-          {links}
+          {smallDeviceLinks}
 
           {/* Mobile Buttons */}
           <div className="flex flex-col space-y-2">
@@ -167,13 +272,13 @@ const Topbar = () => {
               href="/signUp"
               className="hover:bg-blue-600 hover:text-white px-4 py-2 rounded-md border font-semibold flex items-center gap-2 duration-300"
             >
-              <FaRegUser /> Register
+              <User /> Register
             </Link>
             <Link
               href="/login"
               className="bg-blue-600 text-white hover:bg-white hover:text-blue-600 px-4 py-2 rounded-md border font-semibold flex items-center gap-2 duration-300"
             >
-              <IoLockClosedOutline className="font-extra-bold text-16" /> Login
+              <Lock className="font-extra-bold text-16" /> Login
             </Link>
           </div>
         </div>
