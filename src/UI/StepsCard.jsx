4fe9899d5d81a7578b1c27402f64d35431a106f7
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-10-09 12:10:57 +0600

    Created how it works section

---
+++ src/UI/StepsCard.jsx
@@ -0,0 +1,18 @@
+
+const StepsCard = ({Icon, title, description}) => {
+  return (
+    <div className="flex items-start space-x-4">
+      <div className="bg-blue-100 p-3 rounded-md">
+        <Icon className="text-blue-500" size={32} />
+      </div>
+      <div>
+        <h4 className="text-lg font-semibold mb-4">{title}</h4>
+        <p className="text-gray-600">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default StepsCard;
