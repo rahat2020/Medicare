b200a22083754a602ef469202fbb4fafa0e794d3
Mohammad Dinar <web.dev.dinar@gmail.com>
2024-10-22 20:07:50 +0600

    Created product section for landing page

--- src/components/Home/Products.jsx
+++ src/components/Home/Products.jsx
@@ -1,10 +1,22 @@
 import ComponentHeader from '@/UI/ComponentHeader'
 import React from 'react'
+import { products } from '@/data/productData'
+import ProductCard from '@/UI/ProductCard'
+import AppButton from '@/UI/AppButton'
 
 const Products = () => {
+
   return (
     <div className='py-12 px-8'>
-      <ComponentHeader title={"Products"} desc={"Get doctors prescribed all products"} />
+      <ComponentHeader title={"Products"} desc={"Get doctors prescribed products"} />
+      <div className='grid grid-cols-1 md:grid-cols-4 gap-6'>
+        {
+          products?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        }
+      </div>
+      <AppButton href={"/products"} text={"View All"} customStyles={"w-fit flex justify-center mx-auto mt-12 bg-blue-500 hover:bg-blue-600 text-white"}/>
     </div>
   )
 }
