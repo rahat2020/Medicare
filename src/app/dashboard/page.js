646896de0a062bb81f5d39e75f96ae5977835c78
Jisan Molla <jisanahamed09205@gmail.com>
2024-11-19 11:30:07 +0600

    dashboard route initiate

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PatientDashboard = () => {
+    return (
+        <div>
+        <h1>Welcome to Ur Dashboard</h1>
+        </div>
+    );
+};
+
+export default PatientDashboard;
